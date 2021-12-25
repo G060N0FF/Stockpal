ea4c27b85f0c44915c9d770c7e4b0748e5dd4fb2
gdimitrov <georgi.hr.dim@gmail.com>
2021-12-25 16:11:42 +0200

    navbar + logo

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default class Layout extends React.Component {
     render() {
         return (
             <div className='layout'>
-                <h1>Hello world!</h1>
+                <div className='navbar'>
+                    <Link to='/'>
+                        <div className='logo'>
+                            <h2 className='logo-text'>Stockpal</h2>
+                        </div>
+                    </Link>
+                </div>
             </div>
         );
     }
