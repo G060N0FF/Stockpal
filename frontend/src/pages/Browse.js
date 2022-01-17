cd28d72e846faca55610d6f4a2c362c0c2d779c6
gdimitrov <georgi.hr.dim@gmail.com>
2022-01-17 15:46:55 +0200

    Input query filter for chart symbols

--- frontend/src/pages/Browse.js
+++ frontend/src/pages/Browse.js
@@ -8,26 +8,35 @@ export default class Browse extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
+        this.setState({ loaded: true, symbols: ["TSLA", "AAPL", "AMZN", "FB", "NFLX", "GOOGL", "MSFT"] });
+        console.log(this);
+    }
+
+    handleChange(event) {
         this.setState({ loaded: false });
-        const query = "aa";
 
         // find stock symbols by query
-        fetch(`https://ticker-2e1ica8b9.now.sh/keyword/${query}`)
+        fetch(`https://ticker-2e1ica8b9.now.sh/keyword/${event.target.value}`)
         .then(res => res.json())
-        .then(json => this.setState({ symbols: json, loaded: true }));
+        .then(json => json.map((row) => {
+            return row.symbol;
+        }))
+        .then(symbols => this.setState({ symbols: symbols, loaded: true }));
     }
 
     render() {
         return (
             <div className="browse">
+                <input type="text" onChange={this.handleChange} />
                 {
                     this.state.loaded
                     ? (
-                        this.state.symbols.map((row) => {
-                            return <ChartComponent symbol={row.symbol} />;
+                        this.state.symbols.map((symbol) => {
+                            return <ChartComponent symbol={symbol} />;
                         })
                     )
                     : <ClimbingBoxLoader />
